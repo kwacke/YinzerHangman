38f34cde13fcda1bc4011b5d0c30eebc6dd5ebe1
Kyle Wacker <kylewwacker@gmail.com>
2024-01-10 14:07:25 -0500

    more changes

---
+++ YinzerHangman/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Text;
+namespace YinzerHangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Yinzer Hangman, the game where we take everyday words, spell them in Pittsburghese, and have you guess them before you become hanged!");
+
+            string[] yinzWords = {
+                "shahr",
+                "yinz",
+                "clahdy",
+                "jagoff",
+                "dahntahn",
+                "grahnd bees",
+                "rahndabaht",
+                "slippy",
+                "gumband",
+                "seent",
+                "cousint",
+                "friggin",
+                "crick",
+                "arn",
+                "worsh",
+                "sweeper",
+                "spickett",
+                "prolly",
+                "zak same",
+                "lahsy",
+                "jagger bush",
+                "up air",
+                "blesshew",
+                "bo da yous"
+            };
+
+            int incorrect = 0;
+            int correct = 0;
+
+            Console.WriteLine();
+            var random = new Random();
+            var word = random.Next(yinzWords.Length);
+            string answer = yinzWords[word];
+            string playAgain = "";
+            //StringBuilder newWord = new StringBuilder(word);
+
+            //loop while ? while incorrect >= 5 
+            // Condition if user guess full word
+            while (answer.Length > correct || playAgain == "y")
+            {
+                Console.WriteLine($"Your word is {answer.Length} letters long!");
+                Console.WriteLine();
+                Console.WriteLine("Guess your a letter, or if you know the answer, spell out the whole word!");
+
+                string guess = Console.ReadLine();
+
+                try
+                {
+                    if (guess.Length > 1 && guess.Length == answer.Length)
+                    {
+                        if (guess.ToLower() != answer.ToLower())
+                        {
+                            Console.WriteLine("I'm sorry, that was an incorrect guess!");
+                        }
+                        // They guessed the correct word and win the game.
+                        else
+                        {
+                            Console.WriteLine("You win the game!");
+                            correct = guess.Length;
+                        }
+                    }
+                    //user only guessed a single letter
+                    else if (guess.Length == 1)
+                    {
+                        foreach (char singleAnswer in answer)
+                        {
+                            if (guess.Contains(singleAnswer))
+                            {
+                                Console.WriteLine($"{guess} was found in the word! Make your next letter guess or enter word!");
+                                correct++;
+                                if (correct == answer.Length)
+                                {
+                                    Console.WriteLine($"The word is {answer}. You win!");
+                                }
+                            }
+                        }
+                    }
+                Console.WriteLine("Would you like to play again?");
+                Console.WriteLine();
+                Console.WriteLine("'Y' to play again, 'N' to exit");
+                Console.WriteLine();
+                playAgain = Console.ReadLine();
+
+                // User will now select 
+                if (playAgain.ToLower() == "y")
+                {
+                    playAgain = "y";
+                }
+                // User chose not to play again, exit the screen.
+                else if (playAgain.ToLower() == "n")
+                {
+                    playAgain = "n";
+                    Console.WriteLine("Thanks for playing, bye!");
+                }
+                // They made an incorrect choice
+                else
+                {
+                    Console.WriteLine("I'm sorry, please enter Y to play again or N to exit");
+                    Console.WriteLine();
+                    playAgain = Console.ReadLine();
+                }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
